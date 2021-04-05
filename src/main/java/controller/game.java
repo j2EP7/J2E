7ba8698f40b88ee31a327a99eee5c25566c18dba
euoc <ebresba@uoc.edu>
2021-04-05 02:48:42 +0200

    algoritmo game

--- src/main/java/controller/game.java
+++ src/main/java/controller/game.java
@@ -27,4 +27,30 @@ public class game extends HttpServlet {
         List<Word> words = wordDAO.readAll();
         return words;
     }
+
+    // Función que prepara cosas
+
+    // Obtener número de palabras de la configuración read param
+
+
+    // Obtiene listado de palabras readAll
+
+
+    // Obtiene de forma aleatoria X número de palabras del listado de palabras
+
+
+    // Una vez que tenemos el array definitivo de palabras
+    // Teniendo en cuenta el tamaño del casillero, se ubicarían las palabras aleatoriamente
+    // Recorremos listado de palabras
+        // Recorremos letras de la palabra
+            // Ubicamos la letra en una posición aleatoria
+    // Volvemos a recorrer el casillero y rellenamos los espacios vacíos con letras
+
+    // Función // Ubicamos la letra en una posición aleatoria
+        // Ubicar la primera letra
+        // Si está ocupada, nos fijamos de que sea la misma letra
+        // Definir la dirección de la palabra en función del espacio que tenga disponible
+        // Si no tiene ninguno busca otra posición donde asignar
+        // Almacenamos la próxima posición
+
 }
