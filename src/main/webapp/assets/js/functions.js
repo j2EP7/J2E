b7c8d72a7d98132ad5396c44de9bc77022a021ba
jmoletr <jmoletr@uoc.edu>
2021-04-11 03:11:17 +0200

    función tachado

--- src/main/webapp/assets/js/functions.js
+++ src/main/webapp/assets/js/functions.js
@@ -1,8 +1,8 @@
 /* Variables globales */
 
-// Número de palabras encontradas
+// Nï¿½mero de palabras encontradas
 wordsFound = 0;
-// Array con la información de las palabras
+// Array con la informaciï¿½n de las palabras
 // Cada elemento es un objeto (Word) con sus propiedades (id, word y description) y una propiedad extra que es un array, el cual contiene las ids de las posiciones de las letras en el tablero
 gameWords = [];
 // Segundos del juego
@@ -14,7 +14,7 @@ selectedLetters = [];
 
 /* Funciones */
 
-// Función jugar
+// Funciï¿½n jugar
 
 
 /*{
@@ -32,7 +32,7 @@ selectedLetters = [];
 
 
 function play(){
-    // Recoge parámetros para el juego
+    // Recoge parï¿½metros para el juego
     // https://stackoverflow.com/questions/43895473/promise-inside-promise-whats-the-correct-way-to-return-a-variable-from-the-chi/43895627
     jQuery.ajax({
         type: 'POST',
@@ -45,19 +45,19 @@ function play(){
         });
 }
 
-// Ejemplo petición Ajax a servlet
-// petición post vía ajax cuya a un servlet para obtener la estructura de la sopa de letras
-// sería un post a un servlet
-// ese servlet generaría la estructura de la sopa de letras completa y devuelve
+// Ejemplo peticiï¿½n Ajax a servlet
+// peticiï¿½n post vï¿½a ajax cuya a un servlet para obtener la estructura de la sopa de letras
+// serï¿½a un post a un servlet
+// ese servlet generarï¿½a la estructura de la sopa de letras completa y devuelve
 // en la respuesta se inserta la sopa de letras
 function demoAjaxServletRequest(){
 
 }
 
-// Función para comprobar las letras seleccionadas
+// Funciï¿½n para comprobar las letras seleccionadas
 function checkLetters(letraId) {
 
-    // Recorrer todas las palabras (words) y verificar si la posición de esas letras forman una palabra
+    // Recorrer todas las palabras (words) y verificar si la posiciï¿½n de esas letras forman una palabra
     for (let i = 0; i < gameWords.length; i++){
         // Palabra
         let word = gameWords[i];
@@ -69,9 +69,9 @@ function checkLetters(letraId) {
             let letter = letters[j];
             let position = letter.position;
             let positions = [position[0],position[1]];
-            // Si la posición de la letra de la palabra que estamos recorriendo no está en las posiciones almacenadas de las letras seleccionadas
+            // Si la posiciï¿½n de la letra de la palabra que estamos recorriendo no estï¿½ en las posiciones almacenadas de las letras seleccionadas
             if(!selectedLetters.includes(positions)){
-                // Salimos del for, porque esta palabra no está completa
+                // Salimos del for, porque esta palabra no estï¿½ completa
                break;
             }
             // Si se ha llegado al final y tenemos todas las posiciones de las letras de la palabra
@@ -89,14 +89,14 @@ function checkLetters(letraId) {
     }
 }
 
-// Recibe como parámetro el identificador de una palabra que está dentro del array global gameWords
+// Recibe como parï¿½metro el identificador de una palabra que estï¿½ dentro del array global gameWords
 function blockWord(letters){
 
 
     // recorremos gameWords
     // obtenemos palabra por id
     // recorremos letras
-    // bloqueamos la posición de la sopa de letars asignando
+    // bloqueamos la posiciï¿½n de la sopa de letars asignando
     // Asignando la clase letterOff
     for (let i=0;i<letters.length;i++){
         let letra = letters[i].Letter;
@@ -110,12 +110,14 @@ function blockWord(letters){
 }
 
 // Recibe elemento word con propiedades del modelo
-function overlineWord(){
-    // tacha la palabra del listado de palabras
+function overlineWord(word){
+    //Obtenemos palabra cuyo identificador es "Word.id"
+    let palabra = document.getElementById(word.id);
+    //AÃ±adimos la clase css "tachar palabra" a palabra
+    palabra.classList.add('lineThrough');
 }
 
-
-// Función para comprobar si las letras seleccionadas forman una palabra
+// Funciï¿½n para comprobar si las letras seleccionadas forman una palabra
 function lettersAreWord(){
     // Obtenemos las letras seleccionadas
     // Recorremos y obtenemos el identificador del casillero
@@ -124,43 +126,43 @@ function lettersAreWord(){
     // En caso contrario false
 }
 
-// Función para desactivar letras seleccionadas del tablero
+// Funciï¿½n para desactivar letras seleccionadas del tablero
 function disableSelectedLetters(){
-    // Desactiva las letras que están actualmente marcadas como seleccionadas
+    // Desactiva las letras que estï¿½n actualmente marcadas como seleccionadas
 }
 
-// Función contabilizar palabra encontrada
+// Funciï¿½n contabilizar palabra encontrada
 function wordFound(){
     // Aumenta en 1 el contador de palabras encontradas
     wordsFound = wordsFound + 1;
     checkWin();
 }
 
-// Función checkWin
+// Funciï¿½n checkWin
 function checkWin(){
-    // Comprueba si ya están encontradas todas las palabras
+    // Comprueba si ya estï¿½n encontradas todas las palabras
     if(wordsFound == gameWords.length){
         // Finalizar partida
         finishGame();
     }
 }
 
-// Función mostrar victoria
+// Funciï¿½n mostrar victoria
 function youWin(){
     // Muestra mensaje al usuario de que ha ganado
-    const message = "¡Enhorabuena! Has ganado.";
+    const message = "ï¿½Enhorabuena! Has ganado.";
     renderMessage(message);
-    // Renderizamos información de palabras
+    // Renderizamos informaciï¿½n de palabras
     renderWordDetails();
 }
 
-// Función terminar la partida
+// Funciï¿½n terminar la partida
 function finishGame(){
     // Finaliza setInterval
     clearInterval(countdown);
     // Deshabilitamos casillero
     disableGame();
-    // Comprueba número de palabras encontradas
+    // Comprueba nï¿½mero de palabras encontradas
     if(wordsFound == gameWords.length){
         // Si el jugador ha encontrado todas le decimos que ha ganado
         youWin();
@@ -168,44 +170,44 @@ function finishGame(){
         // En caso contrario le decimos que ha perdido
         gameOver();
     }
-    // Habilitamos el botón para que pueda volver a jugar
+    // Habilitamos el botï¿½n para que pueda volver a jugar
     enablePlayButton();
 }
 
-// Función para deshabilitar todas las casillas
+// Funciï¿½n para deshabilitar todas las casillas
 function disableGame(){
     // Para que el jugador no pueda seguir interactuando con las casillas cuando la partida ha finalizado
     jQuery("#game").addClass("gameOff");
 }
 
-// Función para mostrar mensaje
+// Funciï¿½n para mostrar mensaje
 function renderMessage(message){
-    // Recibe el parámetro de las funciones youWin y gameOver
+    // Recibe el parï¿½metro de las funciones youWin y gameOver
     // Renderiza el mensaje en pantalla
     alert(message);
 }
 
 function disablePlayButton(){
-    // Deshabilitar el botón jugar
+    // Deshabilitar el botï¿½n jugar
     document.getElementById("play").disabled = true;
 }
 
 function enablePlayButton(){
-    // Deshabilitar el botón jugar
+    // Deshabilitar el botï¿½n jugar
     document.getElementById("play").disabled = false;
 }
 
-// Función iniciar juego
+// Funciï¿½n iniciar juego
 function initGame(juego){
     // Palabras con letras y posiciones
     gameWords = juego.words;
-    // Set número de palabras a encontrar
+    // Set nï¿½mero de palabras a encontrar
     wordsFound = juego.words.length;
     // Set gameSeconds
     gameSeconds = parseInt(juego.seconds);
     // Renderizamos tablero
     renderGame(juego.casillero);
-    // Deshabilitar botón jugar
+    // Deshabilitar botï¿½n jugar
     disablePlayButton();
     // Renderizamos palabras a encontrar
     renderWords(juego.words);
@@ -222,7 +224,7 @@ function initGame(juego){
         // Obtenemos posiciones de la letra clicada
         let row = letraPosicion[0];
         let col = letraPosicion[1];
-        // Si la letra ha sido seleccionada añadimos a selectedLetters
+        // Si la letra ha sido seleccionada aï¿½adimos a selectedLetters
         if(jQuery("#"+letraId).hasClass("soup")){
             let tempArray = [row,col]
             selectedLetters.push(tempArray);
@@ -240,11 +242,11 @@ function initGame(juego){
     });
 }
 
-// Función para contar el tiempo restante
+// Funciï¿½n para contar el tiempo restante
 function initCountdown(){
-    // Cuenta regresiva con método setInterval
+    // Cuenta regresiva con mï¿½todo setInterval
     // El set interval se ejecuta cada 1000 milisegundos = 1 segundo
-    // Vamos restando 1 segundo por ejecución
+    // Vamos restando 1 segundo por ejecuciï¿½n
     countdown = setInterval(function(){
         gameSeconds = gameSeconds - 1;
         // Muestra segundos restantes en pantalla
@@ -274,14 +276,14 @@ function secondsToTime(seconds){
     return time;
 }
 
-// Función mostrar derrota
+// Funciï¿½n mostrar derrota
 function gameOver(){
     // Muestra mensaje al usuario de que ha perdido
-    const message = "¡Vaya! Se ha agotado el tiempo.";
+    const message = "ï¿½Vaya! Se ha agotado el tiempo.";
     renderMessage(message);
 }
 
-// Función para mostrar palabras a encontrar
+// Funciï¿½n para mostrar palabras a encontrar
 function renderWords(words){
     console.log(words);
     // Obtiene listado de palabras
@@ -301,12 +303,12 @@ function generateWordList(words){
     return html;
 }
 
-// Función para mostrar palabras y descripciones
+// Funciï¿½n para mostrar palabras y descripciones
 function renderWordDetails(){
 
 }
 
-// Función para renderizar la sopa de letras
+// Funciï¿½n para renderizar la sopa de letras
 function renderGame(game){
     document.getElementById('game').innerHTML = game;
 }
